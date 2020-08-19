31afad9b7a102dff90527556a4ebda3de89c1d28
Will Preble <wpreble1@gmail.com>
2020-08-19 08:49:19 -0700

    Add gatsby source strapi plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
-});
+})
 
 module.exports = {
   siteMetadata: {
-    title: `My Super Blog`,
-    description: `Gatsby blog with Strapi`,
-    author: `Strapi team`,
+    title: "My super blog",
+    description: "Gatsby blog with Strapi",
+    author: "Strapi team",
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -29,19 +29,19 @@ module.exports = {
         queryLimit: 1000,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
       },
     },
-    `gatsby-plugin-offline`,
+    "gatsby-plugin-offline",
   ],
 }
